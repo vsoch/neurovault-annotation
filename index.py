77c97ab3a076763da06bc37b3f97b4a3cfd1f514
vsoch <vsochat@stanford.edu>
2015-08-27 17:40:49 -0700

    updating interface with collection level annotation

--- index.py
+++ index.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
-from pybraincompare.report.colors import random_colors
 from hypothesis import HypothesisRawAnnotation
 import requests
+import numpy
 import json
+import re
 import pandas
 
 app = Flask(__name__)
@@ -16,21 +17,30 @@ def get_annotations(urls,column_names):
         text = requests.get(url).text.decode('utf-8')
         rows = json.loads(text)['rows']
         raw = [HypothesisRawAnnotation(row) for row in rows]        
-        annots = dict()
-        # Very simple - will only allow for one annotation per tag
-        # Also assumes one tag per annotation
-        # We can obviously improve upon this!
+        annots = []        
         for r in raw:
-            try: # A user may have done an annotation without a tag
-                if r.tags[0] in column_names:
-                    annots[r.tags[0]] = r.text
-            except:
-                pass
+            data = dict()
+            # Find tags, and ids
+            ids = [id for id in [re.findall("^-?[0-9]+$",tag) for tag in r.tags] if id]
+            if ids:
+                ids = ids[0]
+            else:
+                ids = []
+            tags = [tag for tag in r.tags if tag not in ids]
+            if len(ids)==0: ids = None
+            # We are only taking the first id annotation, one image per annotation
+            data["image_id"] = ids
+            data["tags"] = [{tag:r.text} for tag in tags]
+            annots.append(data)
         if len(annots) > 0:
             annotations[u] = annots
     return annotations    
 
-def get_collections():
+def get_images(pk):
+    url = "http://neurovault.org/api/collections/%s/images/?format=json" %pk    
+    return json.loads(requests.get(url).text.decode('utf-8'))
+
+def get_collections(pk=None):
     # Retrieve neurovault images, sort
     pkl = "static/data/nv_collections.pkl"
     collections = pandas.read_pickle(pkl)
@@ -38,17 +48,124 @@ def get_collections():
     
     # Remove more proprietary stuffs
     collections =  collections.drop(["owner","add_date","contributors"],axis=1)
+    if pk:
+        collections = collections[collections.collection_id==int(pk)]
     return collections
 
+# Change nan values to None to render correctly in interface
+def nan_to_none(field):
+    try:
+        if numpy.isnan(field):
+            return None
+    except:
+        pass
+    return field
+
+# Important fields
+def get_important_fields(images,coll):
+    metadata = []
+    missing = 0
+    present = 0
+    for image in images["results"]:
+        smoothness = nan_to_none(image["smoothness_fwhm"])
+        image_metadata = {"figure":image["figure"],
+                         "cognitive_paradigm_cogatlas":image["cognitive_paradigm_cogatlas"],
+                         "contrast_definition":image["contrast_definition"],
+                         "image_type":image["image_type"],
+                         "modality":image["modality"],
+                         "name":image["name"],
+                         "map_type":image["map_type"],
+                         "smoothness_fwhm":smoothness,
+                         "thumbnail":image["thumbnail"],
+                         "url":image["url"],
+                         "id":image["id"]}
+        missing += sum(x is None for x in image_metadata.values())
+        present += sum(x is not None for x in image_metadata.values())
+        metadata.append(image_metadata)
+
+    subjects = nan_to_none(coll["number_of_subjects"].values[0])
+
+    collection = {"coordinate_space":coll["coordinate_space"].values[0],
+            "software_package":coll["software_package"].values[0],
+            "used_motion_correction":coll["used_motion_correction"].values[0],
+            "number_of_subjects":subjects,
+            "name":coll["name"].values[0],
+            "url":coll["url"].values[0],
+            "journal":coll["journal_name"].values[0],
+            "authors":coll["authors"].values[0],
+            "id":coll["collection_id"].values[0]}
+
+    missing = (sum(x is None for x in collection.values())) + missing
+    present = (sum(x is None for x in collection.values())) + present
+    collection["missing"] = missing
+    collection["present"] = present
+    return {"images":metadata,"collection":collection}
+
+# Match annotations to fields
+# Defined fields will not be over-written by annotations
+# We can only match images to annotations based on imageID tags
+def update_fields(annotations,fields):
+    annots = annotations.values()[0]
+    for annot in annots:
+        # An image annotation
+        if annot["image_id"] != None:
+             idx = [x for x in range(0,len(fields["images"])) if fields["images"][x]["id"] == int(annot["image_id"][0])][0]
+             fields["images"][idx] = update_field(annot,fields["images"][idx])
+        
+        # A collection annotation
+        else:
+            fields["collection"] = update_field(annot,fields["collection"])
+    return fields
+
+# Update a single field
+def update_field(annot,fields):
+    for group in annot["tags"]:
+        for tag,val in group.iteritems():
+            try:
+                if not fields[tag]:
+                    fields[tag] = val
+            except:
+                pass
+    return fields
+
+# 
+def update_annotations(url,collection,pk):
+    annots = get_annotations(url,collection.columns)
+    # Get images using the neurovault API
+    images = get_images(pk)
+    # Get important image and collection fields
+    fields = get_important_fields(images,collection)
+
+    # Match annotations to fields
+    if len(annots) == 0:
+        annots[url] = [{"image_id":None,
+                       "tags":[{"No annotations found!":""}]}] 
+    else:
+        fields = update_fields(annots,fields)
+    return annots,images,fields
+
+# Single collection view
+@app.route("/collection/<pk>")
+def collection(pk):
+    collection = get_collections(pk=pk)
+    # Get annotations for the pk
+    url = collection["url"].tolist()[0]
+    annots,images,fields = update_annotations(url,collection,pk)
+    return render_template("collection.html",
+                           images=images,
+                           annotations=annots,
+                           fields=fields)
+
+# Show annotations for a collection
 @app.route("/annotate/<pk>")
 def annotate(pk):
     collections = get_collections()
+    collection = get_collections(pk)
     # Get annotations for the pk
-    url = collections["url"][collections["collection_id"]==int(pk)].tolist()[0]
-    annots = get_annotations(url,collections.columns)
-    if len(annots) == 0:
-        annots[url] = {"No annotations found!":""}    
-    return main_page(collections,annotations=annots)
+    url = collection["url"].tolist()[0]
+    annots = get_annotations(url,collection.columns)
+    annots,images,fields = update_annotations(url,collection,pk)
+    return main_page(collections,annotations=annots,fields=fields)
 
 @app.route("/faq")
 def faq():
@@ -60,14 +177,7 @@ def annotate_nv():
     collections = get_collections()
     return main_page(collections)
  
-def main_page(collections,annotations=None):
-
-    print annotations
-    # We don't need to annotate these
-    not_annotations = ["url","collection_id","DOI"]
-    total_annotations = len(collections.columns) - len(not_annotations)
-    collections["missing_annotations"] = total_annotations - collections.count(axis=1)
-    collections["defined_annotations"] = collections.count(axis=1)
+def main_page(collections,annotations=None,fields=None):
 
     # Convert (most) columns to strings
     for col in collections.columns:
@@ -86,7 +196,7 @@ def main_page(collections,annotations=None):
 
     # render images with contrasts tagged
     if annotations != None:
-        return render_template("index.html",collections=lists,annotations=annotations)
+        return render_template("index.html",collections=lists,annotations=annotations,fields=fields)
     return render_template("index.html",collections=lists)
 
 if __name__ == "__main__":
